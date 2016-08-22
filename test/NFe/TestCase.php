c6ed185539c43d4c46b4d6a78ab9ba0282f19e7c
Gabriel Marquez <gabriel@conube.com.br>
2016-08-22 11:14:19 -0300

    Removed unused lines

--- test/NFe/TestCase.php
+++ test/NFe/TestCase.php
@@ -1,13 +1,8 @@
 <?php
 
 class NFe_TestCase extends UnitTestCase {
-  const API_KEY = 'yucG1xbixd2SxjihAwmTKnx7z9pmD9NwfMYxzCVWDx3G999NAkC6LpBnqqWQoXScMWl';
-
   public function __construct() {
     $apiKey = getenv('NFE_API_KEY');
-    if ( ! $apiKey ) {
-      $apiKey = self::API_KEY;
-    }
     NFe::setApiKey($apiKey);
   }
 }
