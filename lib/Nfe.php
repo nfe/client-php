13b787eb977f174cfe2905ecc2be48ac8edf02e0
Renato Alves <espellcaste@gmail.com>
2017-01-22 11:51:16 -0300

    Changing variable to a smaller one

--- lib/Nfe.php
+++ lib/Nfe.php
@@ -1,22 +1,22 @@
 <?php
 
-$baseDirectory = dirname(__FILE__);
+$dir = dirname(__FILE__);
 
-require( $baseDirectory . "/NFe/Backward_Compatibility.php" );
+require( $dir . "/NFe/Backward_Compatibility.php" );
 
-require( $baseDirectory . "/NFe/Base.php" );
-require( $baseDirectory . "/NFe/SearchResult.php" );
-require( $baseDirectory . "/NFe/Object.php" );
-require( $baseDirectory . "/NFe/Utilities.php" );
+require( $dir . "/NFe/Base.php" );
+require( $dir . "/NFe/SearchResult.php" );
+require( $dir . "/NFe/Object.php" );
+require( $dir . "/NFe/Utilities.php" );
 
-require( $baseDirectory . "/NFe/APIRequest.php" );
-require( $baseDirectory . "/NFe/APIResource.php" );
-require( $baseDirectory . "/NFe/APIChildResource.php" );
+require( $dir . "/NFe/APIRequest.php" );
+require( $dir . "/NFe/APIResource.php" );
+require( $dir . "/NFe/APIChildResource.php" );
 
-require( $baseDirectory . "/NFe/Company.php" );
-require( $baseDirectory . "/NFe/LegalPerson.php" );
-require( $baseDirectory . "/NFe/NaturalPerson.php" );
-require( $baseDirectory . "/NFe/Webhook.php" );
-require( $baseDirectory . "/NFe/ServiceInvoice.php" );
+require( $dir . "/NFe/Company.php" );
+require( $dir . "/NFe/LegalPerson.php" );
+require( $dir . "/NFe/NaturalPerson.php" );
+require( $dir . "/NFe/Webhook.php" );
+require( $dir . "/NFe/ServiceInvoice.php" );
 
-require( $baseDirectory . "/NFe/Factory.php" );
+require( $dir . "/NFe/Factory.php" );
